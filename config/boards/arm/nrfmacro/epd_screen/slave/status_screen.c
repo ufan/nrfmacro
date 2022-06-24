bddff3e240bc845bafbf5f1e04d47813e81b5265
Yong Zhou <zyong06@gmail.com>
2022-06-25 07:32:13 +0800

    add dedicated peripheral and battery status widget for epd_screen

--- config/boards/arm/nrfmacro/epd_screen/slave/status_screen.c
+++ config/boards/arm/nrfmacro/epd_screen/slave/status_screen.c
@@ -4,18 +4,18 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <zmk/display/widgets/peripheral_status.h>
-#include <zmk/display/widgets/battery_status.h>
+#include "peripheral_status.h"
+#include "battery_status.h"
 #include <zmk/display/status_screen.h>
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
 static struct zmk_widget_battery_status battery_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
 static struct zmk_widget_peripheral_status peripheral_status_widget;
 #endif
 
@@ -24,17 +24,22 @@ lv_obj_t *zmk_display_status_screen() {
 
     screen = lv_obj_create(NULL, NULL);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT,
                  0, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
     lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), NULL,
                  LV_ALIGN_IN_TOP_LEFT, 0, 0);
 #endif
+
+    // todo:
+    // 1. a product mark line (sweep-pro)
+    // 2. a product logo
+    // 3. configurable personal logo, which can replace the product logo
 
     return screen;
 }
